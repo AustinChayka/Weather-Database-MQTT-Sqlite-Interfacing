f3b9eff0e99847d974d9d7b5e3db6d319526376b
Austin Chayka <austinchayka@gmail.com>
2019-05-17 14:08:56 -0700

    Clean-Up
    file clean-up

--- DB_Functions.py
+++ DB_Functions.py
@@ -1,10 +1,20 @@
 import sqlite3
 
+#   make_entry(cursor, temperature, light_level, pressure, humidity, date = None)
+#       cursor - SQLite3 cursor object
+#       temperature - temperature reading as float
+#       light_level - light reading as float
+#       pressure - pressure reading as float
+#       humidity - humidity reading as float
+#       (optional) date - reading date as string
+#   function makes entry into the database that the cursor is connected to    
 def make_entry(cursor, temperature, light_level, pressure, humidity, date = None):
+    #check values
     assert isinstance(temperature, float), 'make_entry(), temperature in not of value float'
     assert isinstance(light_level, float), 'make_entry(), light level in not of value float'
     assert isinstance(pressure, float), 'make_entry(), pressure in not of value float'
     assert isinstance(humidity, float), 'make_entry(), humidity in not of value float'
+    #check if date value entered and enter data
     if date is not None:
         values = date + ',' + str(temperature) + ',' + str(light_level) + ',' + str(pressure) + ',' + str(humidity)
         print('\n' + values)
@@ -20,25 +30,59 @@ def make_entry(cursor, temperature, light_level, pressure, humidity, date = None
         )
         print('\n' + 'CURRENT_TIMESTAMP,' + values)
 
+#   print_db_definition(cursor, table_name)
+#       cursor - SQLite3 cursor object
+#       table_name - string name of table
+#   prints the table definition
 def print_db_definition(cursor, table_name):
+    #check table_name
     assert isinstance(table_name, str), 'print_db_definition(), table_name is not of value string'
+    #get table info
     cursor.execute('PRAGMA table_info(' + table_name + ')')
+    #print
     print('')
     for row in cursor.fetchall():
         print(row)
 
+#   print_db_entries(cursor, table_name)
+#       cursor - SQLite3 cursor object
+#       table_name - string name of table
+#   prints all database entries
 def print_db_entries(cursor, table_name):
+    #check table_name
     assert isinstance(table_name, str), 'print_db_entries(), table_name is not of value string'
+    #get entries
     cursor.execute('SELECT * FROM ' + table_name)
+    #print
     print('')
     for entry in cursor.fetchall():
         print(entry)
 
+#   parse_mqtt_payload(payload)
+#       payload - MQTT message payload string
+#   parses the message payload into data list
 def parse_mqtt_payload(payload):
+    #check payload
     assert isinstance(payload, str), 'parse_mqtt_payload(), payload is not of type string'
+    #cut padding
     payload = payload[2:-1]
+    #split into array
     output = payload.split(',')
+    #check number of data entries
     assert len(output) <= 5, 'parse_mqtt_payload(), payload has too many entries'
+    #convert values
     for i in range(len(output)):
-        output[i] = float(output[i])
+        if(i != 5):
+            output[i] = float(output[i])
     return output
+
+#   print_latest_entry(cursor, table_name)
+#       cursor - SQLite3 cursor object
+#   prints the newest database entry
+def print_latest_entry(cursor, table_name):
+    #check table_name
+    assert isinstance(table_name, str), 'print_latest_entry(), table_name is not of type string'
+    #get data
+    cursor.execute('SELECT * FROM ' + table_name + ' WHERE id = (SELECT MAX(id) FROM ' + table_name + ')')
+    #print
+    print(cursor.fetchall()[0])
--- MQTT Subscribe.py
+++ MQTT Subscribe.py
@@ -2,9 +2,11 @@ import paho.mqtt.client as mqtt
 import DB_Functions as dbf
 import sqlite3
 
+#connect to database
 conn = sqlite3.connect('WeatherData.db')
 cursor = conn.cursor()
 
+#create table if it doesn't exist
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS readings (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,32 +18,40 @@ cursor.execute('''
     );
 ''')
 
+#print database definition
 dbf.print_db_definition(cursor, 'readings')
 
+#define new on_connect function
 def on_connect(client, userdata, flags, rc):
+    #print connection
     print("\nConnected with result code " + str(rc))
+    #subscribe to topic
+    client.subscribe("sensorData")
 
-    client.subscribe("python/test")
-
+#define new on_message function
 def on_message(client, userdata, msg):
+    #print message
     print('')
     print(msg.topic + " " + str(msg.payload))
     data = dbf.parse_mqtt_payload(str(msg.payload))
     print(data)
-
+    #parse message data
     if(len(data) == 5):
         dbf.make_entry(cursor, data[0], data[1], data[2], data[3], data[4])
         print('data entered')
     else:
         dbf.make_entry(cursor, data[0], data[1], data[2], data[3])
         print('data entered')
+    #enter data into database
+    dbf.print_latest_entry(cursor, 'readings')
 
-    dbf.print_db_entries(cursor, 'readings')
-
+#create new MQTT client object
 client = mqtt.Client()
+#set function definitions
 client.on_connect = on_connect
 client.on_message = on_message
 
+#connect to broker
 client.connect("10.6.37.100", 1883)
-
+#start loop
 client.loop_forever()
